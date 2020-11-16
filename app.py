424928a9f2d357d0e0c2bdfeaeeb7dc18787a6ef
Frenz86 <frenzy86@gmail.com>
2020-11-16 09:39:50 +0100

    ww

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ st.table(to_show)
 
 st.markdown(
     '''
-    <iframe src='https://flo.uri.sh/visualisation/1889889/embed' frameborder='0' scrolling='no' style='width:100%;height:600px;'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'></div>
+    <iframe src='https://flo.uri.sh/visualisation/1627129/embed' title='Interactive or visual content' frameborder='0' scrolling='no' style='width:100%;height:600px;'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/1627129/?utm_source=embed&utm_campaign=visualisation/1627129' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
     ''',
     unsafe_allow_html=True
 )
