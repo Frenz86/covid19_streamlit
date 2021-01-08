77a1dc61335b3651c74fa60c62e8b02d84ca0ab9
Frenz86 <frenzy86@gmail.com>
2021-01-08 13:38:57 +0100

    ww

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ st.markdown(
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <title>COVID-19 Dashboard | Hetav Desai</title>
+  <title>COVID 20-21 Dashboard | Hetav Desai</title>
   <style>
   body{
       background-color: #fff;
